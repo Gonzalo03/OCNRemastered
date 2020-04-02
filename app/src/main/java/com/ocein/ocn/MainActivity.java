286721f228ee1e3bb04becd37511fc7b861be0e7
Gox <gonzagod19@gmail.com>
2020-04-02 18:40:36 -0500

    Primer avance

---
+++ app/src/main/java/com/ocein/ocn/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ocein.ocn;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button guardar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        guardar = findViewById(R.id.guardar);
+
+        guardar.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
